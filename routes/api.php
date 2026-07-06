ad544d751c9780077da7998d0b219771570f2973
najwa13 <najwafathi05@hotmail.com>
2026-07-06 12:36:54 +0100

    feat(posts): register post routes

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BlueprintController;
 use App\Http\Controllers\Api\TextBrutController;
 use App\Http\Controllers\Api\GeneratePostController;
+use App\Http\Controllers\Api\PostController;
 
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -15,5 +16,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('blueprints', BlueprintController::class);
     Route::apiResource('text-bruts', TextBrutController::class);
-    Route::post('/text-bruts/{textBrut}/generate',[GeneratePostController::class, 'generate']);
+    Route::post('/text-bruts/{textBrut}/generate', [GeneratePostController::class, 'generate']);
+    Route::apiResource('posts', PostController::class);
 });
