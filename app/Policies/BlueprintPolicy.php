73ebb31f986b3e013b0a99f1b276bfb51b76c87b
najwa13 <najwafathi05@hotmail.com>
2026-07-06 12:26:08 +0100

    fix(policy): correct blueprint create method signature

--- app/Policies/BlueprintPolicy.php
+++ app/Policies/BlueprintPolicy.php
@@ -27,9 +27,9 @@ class BlueprintPolicy
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user , Blueprint $blueprint): bool
+    public function create(User $user): bool
     {
-         return $user->id === $blueprint->user_id;
+        return true;
     }
 
     /**
